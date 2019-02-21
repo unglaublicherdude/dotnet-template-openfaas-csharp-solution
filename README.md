e383a10691c2c3a53850c4fd6d62bcb3dbf0cc79
Matthias <gitlab@matthias-simonis.de>
2019-02-21 01:43:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# csharp OpenFaaS Solution Template
+
+This is a csharp OpenFaaS Solution Template containing one function.
---
+++ src/__MyInitialFunctionName__/FunctionHandler.cs
@@ -0,0 +1,7 @@
+namespace __MyInitialFunctionName__
+{
+    public static class FunctionHandler
+    {
+        public static string Handle(string input) => input.ToUpper();
+    }
+}
